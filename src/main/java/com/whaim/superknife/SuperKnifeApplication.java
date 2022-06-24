4818519ea4be0495c83aef0e368997037d692ae9
王海明 <whaim@icloud.com>
2022-06-24 10:11:56 +0800

    init

---
+++ src/main/java/com/whaim/superknife/SuperKnifeApplication.java
@@ -0,0 +1,13 @@
+package com.whaim.superknife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperKnifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperKnifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whaim/superknife/SuperKnifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whaim.superknife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperKnifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
