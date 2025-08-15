531cb6a5a0107aac8e9954d1b02644f8592305fd
whaim <xminsky@gmail.com>
2025-08-15 15:20:55 +0800

    up

--- addon/test.js
+++ addon/test.js
@@ -2,7 +2,7 @@
 // Load the native addon using the 'bindings' module
 // This will look for the compiled .node file in various places
 const bindings = require('bindings')
-const addon = bindings('api')
+const addon = bindings('addon')
 
 
 // Try the verifyLogin function
