a357111053dcf18be427072a16a36919cccf58b9
HmingWang <xminsky@gmail.com>
2025-10-03 13:47:32 +0800

    整理，add logo , 修正xcore

--- cmake/xcore.cmake
+++ cmake/xcore.cmake
@@ -1,15 +1,14 @@
+message(STATUS "Including xCore")
+
 include(FetchContent)
 
-set(XCORE_NAMESPACE "xCore")
-set(FETCHCONTENT_QUIET OFF)
-
-# Set CMP0077 to NEW to allow overriding options
-cmake_policy(SET CMP0077 NEW)
-
-# Configure xCore options before fetching
 set(XCORE_BUILD_TESTS OFF CACHE BOOL "Don't build xCore tests" FORCE)
-set(XCORE_INSTALL OFF CACHE BOOL "Don't install xCore" FORCE)
-set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build static libraries" FORCE)
 
-# Add the external xCore directory directly
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/xcore)
+
+FetchContent_Declare(
+  xCore
+  GIT_REPOSITORY git@hming.wang:xcore.git
+  GIT_TAG        new
+)
+
+FetchContent_MakeAvailable(xCore)
